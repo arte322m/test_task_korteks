d845a5a3f2a696dda7419a42bb6be61acc26b534
Artem <posevin2011@gmail.com>
2023-11-13 23:47:53 +0300

    Справочник должностей (удаление, список, изменение, добавление)

--- test_task_korteks_app/models.py
+++ test_task_korteks_app/models.py
@@ -5,9 +5,9 @@ class Employees(models.Model):
     firstname = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     father_name = models.CharField(max_length=40, null=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    post = models.ForeignKey('JobTitle', on_delete=models.CASCADE)
     employment_date = models.DateField()
 
 
-class Post(models.Model):
+class JobTitle(models.Model):
     name = models.CharField(max_length=50)
