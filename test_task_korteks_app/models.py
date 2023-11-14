dfe9a9340d58bf4c1586182e1a522dea7daa1c4c
Artem <posevin2011@gmail.com>
2023-11-15 00:46:27 +0300

    Список сотрудников(добавление, удаление, изменение)

--- test_task_korteks_app/models.py
+++ test_task_korteks_app/models.py
@@ -5,7 +5,7 @@ class Employee(models.Model):
     firstname = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     father_name = models.CharField(max_length=40, null=True)
-    job_title = models.ForeignKey('JobTitle', on_delete=models.CASCADE)
+    job_title = models.ForeignKey('JobTitle', on_delete=models.CASCADE, null=True)
     employment_date = models.DateField()
 
 
--- test_task_korteks_app/urls.py
+++ test_task_korteks_app/urls.py
@@ -4,9 +4,16 @@ from . import views
 app_name = 'test_task_korteks_app'
 urlpatterns = [
     path('', views.main, name='main'),
+
     path('job_title/', views.job_title_list, name='job_titles_list'),
-    path('job_title/<int:job_title_id>', views.job_title_detail, name='job_title_detail'),
+    path('job_title/<int:job_title_id>/', views.job_title_detail, name='job_title_detail'),
     path('job_title/add/', views.job_title_add, name='job_title_add'),
-    path('job_title/change/<int:job_title_id>', views.job_title_change, name='job_title_change'),
-    path('job_title/delete/<int:job_title_id>', views.delete_job_title, name='delete_job_title'),
+    path('job_title/change/<int:job_title_id>/', views.job_title_change, name='job_title_change'),
+    path('job_title/delete/<int:job_title_id>/', views.job_title_delete, name='job_title_delete'),
+
+    path('employee/', views.employees_list, name='employees_list'),
+    path('employee/<int:employee_id>/', views.employee_detail, name='employee_detail'),
+    path('employee/add/', views.employee_add, name='employee_add'),
+    path('employee/change/<int:employee_id>/', views.employee_change, name='employee_change'),
+    path('employee/delete/<int:employee_id>/', views.employee_delete, name='employee_delete'),
 ]
